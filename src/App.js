ba8afb183ad8b2a93241a225971949bc0a7e0079
김민성 <yssj2049@gmail.com>
2019-01-26 16:26:18 +0900

    Upload App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
 
+const movie = [
+  "Matrix",
+  "Full Metal Jacket",
+  "oldboy",
+  "Star wars"
+]
 class App extends Component {
   render() {
     return (
       <div className="App">
-        hi!
+        <Movie title ={movie[0]}/>
+        <Movie title ={movie[1]}/>
+        <Movie title ={movie[2]}/>
+        <Movie title ={movie[3]}/>
+
       </div>
     );
   }
